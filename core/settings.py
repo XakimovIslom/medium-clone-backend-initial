b1390bd4f8907d7b4fc9db1b080647487b056f4c
Islom <islomhakimov6@gmail.com>
2024-09-01 00:35:40 +0500

    added users app

--- core/settings.py
+++ core/settings.py
@@ -25,6 +25,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     'rest_framework',
+    'users',
 ]
 
 MIDDLEWARE = [
@@ -127,3 +128,4 @@ STATIC_ROOT = BASE_DIR / "static"
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+AUTH_USER_MODEL = 'users.CustomUser'

7504fc56f09b03c1df024d423fa60abc6f0c9928
Islom <islomhakimov6@gmail.com>
2024-09-05 18:25:52 +0500

    added jwt

--- users/urls.py
+++ users/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-
-
+from users import views
 urlpatterns = [
-
+    path('signup/', views.SignupView.as_view(), name='signup'),
+    path('login/', views.LoginView.as_view(), name='login'),
+    path('me/', views.UsersMe.as_view(), name='users-me'),
 ]
